7c3e69056adeb02c46abc7b9a420e1a6a420e71c
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-07-29 17:55:03 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors')
+const bodyParser=require('body-parser')
+const app = express();
+const base64Img = require('base64-img');
+const { v4: uuidv4 } = require('uuid');
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json({limit: '10mb'}));
+
+app.post('/upload',(req,res)=>{
+    const filepath = base64Img.imgSync(req.body.base64, '../images', `${uuidv4()}${Date.now()}`); // Synchronous using
+    res.status(200).json({location:`https://www.brainlytic.org/${filepath.replaceAll('\\','/').replaceAll('../','')}`})
+})
+
+
+const port = process.env.PORT || 4000;
+
+
+app.listen(port);
+console.log(`Server listening at port: ${port}`);
